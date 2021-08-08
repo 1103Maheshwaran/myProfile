72a371988bffad42d59d2d3a2a0a0025e92c9abf
1103Maheshwaran <g.maheshwaran2015@vit.ac.in>
2021-08-08 21:28:17 +0530

    Fixed awsome font,typewritter effect,flex issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,118 +6,159 @@
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$main = $('#main');
+    var $window = $(window),
+        $body = $('body'),
+        $main = $('#main');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+    // Breakpoints.
+    breakpoints({
+        xlarge: ['1281px', '1680px'],
+        large: ['981px', '1280px'],
+        medium: ['737px', '980px'],
+        small: ['481px', '736px'],
+        xsmall: ['361px', '480px'],
+        xxsmall: [null, '360px']
+    });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Play initial animations on page load.
+    $window.on('load', function() {
+        window.setTimeout(function() {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// Nav.
-		var $nav = $('#nav');
+    // Nav.
+    var $nav = $('#nav');
 
-		if ($nav.length > 0) {
+    if ($nav.length > 0) {
 
-			// Shrink effect.
-				$main
-					.scrollex({
-						mode: 'top',
-						enter: function() {
-							$nav.addClass('alt');
-						},
-						leave: function() {
-							$nav.removeClass('alt');
-						},
-					});
+        // Shrink effect.
+        $main
+            .scrollex({
+                mode: 'top',
+                enter: function() {
+                    $nav.addClass('alt');
+                },
+                leave: function() {
+                    $nav.removeClass('alt');
+                },
+            });
 
-			// Links.
-				var $nav_a = $nav.find('a');
+        // Links.
+        var $nav_a = $nav.find('a');
 
-				$nav_a
-					.scrolly({
-						speed: 1000,
-						offset: function() { return $nav.height(); }
-					})
-					.on('click', function() {
+        $nav_a
+            .scrolly({
+                speed: 1000,
+                offset: function() { return $nav.height(); }
+            })
+            .on('click', function() {
 
-						var $this = $(this);
+                var $this = $(this);
 
-						// External link? Bail.
-							if ($this.attr('href').charAt(0) != '#')
-								return;
+                // External link? Bail.
+                if ($this.attr('href').charAt(0) != '#')
+                    return;
 
-						// Deactivate all links.
-							$nav_a
-								.removeClass('active')
-								.removeClass('active-locked');
+                // Deactivate all links.
+                $nav_a
+                    .removeClass('active')
+                    .removeClass('active-locked');
 
-						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-							$this
-								.addClass('active')
-								.addClass('active-locked');
+                // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+                $this
+                    .addClass('active')
+                    .addClass('active-locked');
 
-					})
-					.each(function() {
+            })
+            .each(function() {
 
-						var	$this = $(this),
-							id = $this.attr('href'),
-							$section = $(id);
+                var $this = $(this),
+                    id = $this.attr('href'),
+                    $section = $(id);
 
-						// No section for this link? Bail.
-							if ($section.length < 1)
-								return;
+                // No section for this link? Bail.
+                if ($section.length < 1)
+                    return;
 
-						// Scrollex.
-							$section.scrollex({
-								mode: 'middle',
-								initialize: function() {
+                // Scrollex.
+                $section.scrollex({
+                    mode: 'middle',
+                    initialize: function() {
 
-									// Deactivate section.
-										if (browser.canUse('transition'))
-											$section.addClass('inactive');
+                        // Deactivate section.
+                        if (browser.canUse('transition'))
+                            $section.addClass('inactive');
 
-								},
-								enter: function() {
+                    },
+                    enter: function() {
 
-									// Activate section.
-										$section.removeClass('inactive');
+                        // Activate section.
+                        $section.removeClass('inactive');
 
-									// No locked links? Deactivate all links and activate this section's one.
-										if ($nav_a.filter('.active-locked').length == 0) {
+                        // No locked links? Deactivate all links and activate this section's one.
+                        if ($nav_a.filter('.active-locked').length == 0) {
 
-											$nav_a.removeClass('active');
-											$this.addClass('active');
+                            $nav_a.removeClass('active');
+                            $this.addClass('active');
 
-										}
+                        }
 
-									// Otherwise, if this section's link is the one that's locked, unlock it.
-										else if ($this.hasClass('active-locked'))
-											$this.removeClass('active-locked');
+                        // Otherwise, if this section's link is the one that's locked, unlock it.
+                        else if ($this.hasClass('active-locked'))
+                            $this.removeClass('active-locked');
 
-								}
-							});
+                    }
+                });
 
-					});
+            });
 
-		}
+    }
 
-	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000
-		});
+    // Scrolly.
+    $('.scrolly').scrolly({
+        speed: 1000
+    });
 
 })(jQuery);
+
+//Testing typewritting
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Data Scientist", "JAVA Developer", "Python Programmer", "Crypto Geek"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        cursorSpan.classList.remove("typing");
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
